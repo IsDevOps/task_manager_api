9b231e3c71bb985a2f60a1db1dc1c19f4a49cb85
ossygarcia <mrbrucekessy@gmail.com>
2023-05-08 23:54:14 +0100

    commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+
+const PORT = process.env.PORT || 5000
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to Task Manager")
+})
+app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
